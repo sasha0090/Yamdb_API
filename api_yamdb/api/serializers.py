ea62c09583abbc229f339294393125a03c611892
Антон Денисов <antohaden83@gmail.com>
2022-07-18 22:31:29 +0300

    Создал новое приожение, исправил ряд моментов

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,29 +1,31 @@
 from rest_framework import serializers
 from djoser.serializers import UserSerializer
-from api.models import User
+from users.models import User
 
 
 class CustomUserSerializer(UserSerializer):
-    ''' Класс для переопределения нового эндпоинта '''
+    """Класс для переопределения нового эндпоинта"""
 
     class Meta:
         model = User
-        fields = (
-            'username', 'email', 'first_name', 'last_name', 'bio', 'role'
-        )
+        fields = ("username",
+                  "email",
+                  "first_name",
+                  "last_name",
+                  "bio",
+                  "role"
+                  )
 
     def validate(self, data):
-        if data.get('username') == 'me':
-            raise serializers.ValidationError(
-                'Username указан неверно!')
+        if data.get("username") == "me":
+            raise serializers.ValidationError("Username указан неверно!")
         return data
 
 
 class AuthSignUpSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
-        fields = ('email', 'username')
+        fields = ("email", "username")
 
 
 class AuthTokenSerializer(serializers.Serializer):
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,17 +1,16 @@
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
-from .import views
+from . import views
 
 
 router = DefaultRouter()
 
-router.register(r'users', views.UserViewSet, basename='users')
-
+router.register(r"users", views.UserViewSet, basename="users")
 
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
-    path('v1/auth/signup', views.signup_new_user, name='signup'),
-    path('v1/auth/token', views.get_token, name='token'),
+    path("v1/", include(router.urls)),
+    path("v1/auth/signup", views.signup_new_user, name="signup"),
+    path("v1/auth/token", views.get_token, name="token"),
 ]
