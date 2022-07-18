b7eb50fafc8c9d921164209402a7e73c583b221f
Антон Денисов <antohaden83@gmail.com>
2022-07-18 22:37:47 +0300

    Создал новое приожение, исправил ряд моментов

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     "users.apps.UsersConfig",
 ]
 
-AUTH_USER_MODEL = "api.User"
+AUTH_USER_MODEL = "users.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
