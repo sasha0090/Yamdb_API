7aef171ed6a3606dd33de92d9d2ab86fc7cdf350
Александр Дзюба <komrad.dzyuba2014@yandex.ru>
2022-07-19 18:28:39 +0300

    Модели, представления, эндпойнты

---
+++ api_yamdb/reviews/views.py
@@ -0,0 +1,21 @@
+# from django.shortcuts import render
+
+from rest_framework import viewsets  # filters, permissions, viewsets
+
+from . import serializers
+from .models import Category, Genre, Title
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = serializers.GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = serializers.TitleSerializer
