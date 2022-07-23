1c833d1b65852efc9afebfb10847ad2126bce18b
Alexander Telepin <19710759+sasha0090@users.noreply.github.com>
2022-07-23 18:56:47 +0300

    Поправил проверку safe methods

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,20 +1,21 @@
 from rest_framework import permissions
 
 
+def is_safe_methods(method):
+    return method in permissions.SAFE_METHODS
+
+
 class IsAuthorOrStaffOrReadOnly(permissions.BasePermission):
     """Просматривать может любой, а запись только автор или админ с модером"""
 
     def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
+        return is_safe_methods(request.method) or request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         user = request.user
 
         return (
-            request.method in permissions.SAFE_METHODS
+            is_safe_methods(request.method)
             or user.is_authenticated
             and (obj.author == user or user.is_moderator or user.is_admin)
         )
