15464b1a663ccfc65705ee77392d61fbd3d5182a
Антон Денисов <antohaden83@gmail.com>
2022-07-24 21:49:22 +0300

    final try

--- api_yamdb/csv_to_db.py
+++ api_yamdb/csv_to_db.py
@@ -1,41 +1,49 @@
 import csv
 import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_yamdb.settings")
 import django
-
-django.setup()
-
 
 from django.contrib.auth import get_user_model
 
 from reviews.models import Category, Comment, Genre, Review, Title
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_yamdb.settings")
+
+
+django.setup()
+
+
 User = get_user_model()
 
 
 data = {
-    "users.csv": {
-        User: "User(id=row['id'],username=row['username'],email=row['email'],role=row['role'])"
-    },
-    "category.csv": {
-        Category: "Category(id=row['id'],name=row['name'],slug=row['slug'])"
-    },
-    "genre.csv": {
-        Genre: "Genre(id=row['id'],name=row['name'],slug=row['slug'])"
-    },
-    "titles.csv": {
-        Title: "Title(id=row['id'],name=row['name'],year=row['year'],category=Category.objects.get(pk=row['category']))"
-    },
-    "genre_title.csv": {
-        Title.genre.through: "Title.genre.through(title_id=row['title_id'], genre_id=row['genre_id'])"
-    },
-    "review.csv": {
-        Review: "Review(id=row['id'],title=Title.objects.get(pk=row['title_id']),text=row['text'],author=User.objects.get(pk=row['author']),score=row['score'],pub_date=row['pub_date'])"
-    },
-    "comments.csv": {
-        Comment: "Comment(id=row['id'],review=Review.objects.get(pk=row['review_id']),text=row['text'],author=User.objects.get(pk=row['author']),pub_date=row['pub_date'])"
-    },
+    'users.csv':
+        {User: "User(id=row['id'],"
+               "username=row['username'],"
+               "email=row['email'],"
+               "role=row['role'])"},
+    'category.csv':
+        {Category: "Category(id=row['id'],name=row['name'],slug=row['slug'])"},
+    'genre.csv':
+        {Genre: "Genre(id=row['id'],name=row['name'],slug=row['slug'])"},
+    'titles.csv': {
+        Title: "Title(id=row['id'],"
+               "name=row['name'],"
+               "year=row['year'],"
+               "category=Category.objects.get(pk=row['category']))"},
+    'genre_title.csv': {
+        Title.genre.through: "Title.genre.through("
+                             "title_id=row['title_id'], "
+                             "genre_id=row['genre_id'])"},
+    'review.csv': {
+        Review: "Review(id=row['id'],"
+                "title=Title.objects.get(pk=row['title_id']),"
+                "text=row['text'],author=User.objects.get(pk=row['author']),"
+                "score=row['score'],pub_date=row['pub_date'])"},
+    'comments.csv': {
+        Comment: "Comment(id=row['id'],"
+                 "review=Review.objects.get(pk=row['review_id']),"
+                 "text=row['text'],author=User.objects.get(pk=row['author']),"
+                 "pub_date=row['pub_date'])"},
 }
 
 
