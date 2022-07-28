4018ba1aaa20a2f61cc109c64081a8c1baeb7618
Alexander Telepin <19710759+sasha0090@users.noreply.github.com>
2022-07-28 17:37:47 +0300

    Поменял readme, т.к. падал тест

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(', ')
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(", ")
 
 
 # Application definition
