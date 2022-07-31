63b229befd17a548328edeab97d5cce2bc984fd8
Антон Денисов <antohaden83@gmail.com>
2022-07-31 19:17:08 +0300

    fix urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,26 +4,26 @@ from rest_framework import routers
 from . import views
 
 app_name = "api"
-router = routers.DefaultRouter()
+router_v1 = routers.DefaultRouter()
 
-router.register(
+router_v1.register(
     r"titles/(?P<title_id>\d+)/reviews",
     views.ReviewViewSet,
     basename="reviews",
 )
-router.register(
+router_v1.register(
     r"titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments",
     views.CommentViewSet,
     basename="comments",
 )
 
-router.register("users", views.UserViewSet, basename="users")
-router.register("titles", views.TitleViewSet, basename="title")
-router.register("categories", views.CategoryViewSet, basename="category")
-router.register("genres", views.GenreViewSet, basename="genre")
+router_v1.register("users", views.UserViewSet, basename="users")
+router_v1.register("titles", views.TitleViewSet, basename="title")
+router_v1.register("categories", views.CategoryViewSet, basename="category")
+router_v1.register("genres", views.GenreViewSet, basename="genre")
 
 urlpatterns = [
-    path("v1/", include(router.urls)),
+    path("v1/", include(router_v1.urls)),
     path("v1/auth/signup/", views.signup, name="signup"),
     path("v1/auth/token/", views.token, name="token"),
 ]
